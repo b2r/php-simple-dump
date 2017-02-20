6ccb29e9751550300b3a00ea7fc7d8e5e89f78b2
b2r <b2r.net@gmail.com>
2017-02-20 13:14:52 +0900

    Update Test

--- Tests/SimpleDumpTest.php
+++ Tests/SimpleDumpTest.php
@@ -4,20 +4,6 @@ use b2r\Component\SimpleDump\SimpleDump;
 
 class SimpleDumpTest extends PHPUnit\Framework\TestCase
 {
-    public function testBasic()
-    {
-        SimpleDump::init();
-        $p = function ($value) {
-            return trim(ps($value));
-        };
-        $this->assertEquals('string(6) "string"', $p('string'));
-        $this->assertEquals('int(1)', $p(1));
-        $this->assertEquals('float(1)', $p(1.0));
-        $this->assertEquals('bool(true)', $p(true));
-        $this->assertEquals('bool(false)', $p(false));
-        $this->assertEquals('NULL', $p(null));
-    }
-
     public function testDumpDisplay()
     {
         SimpleDump::init();
